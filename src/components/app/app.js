ad6b47f5e90e71704cdda9c2c77ba1b9f6a987b4
OrphanJews <911815@gmail.com>
2019-07-02 20:39:42 +0500

    checkpoint

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -9,6 +9,32 @@ export default class App extends React.Component {
         matrixWidth: 10,
         matrixHeight: 10,
         probability: 0.5,
+        domainCount: 0,
+
+        arr: [
+            Array(10).fill(0),
+            Array(10).fill(0),
+            Array(10).fill(0),
+            Array(10).fill(0),
+            Array(10).fill(0),
+            Array(10).fill(0),
+            Array(10).fill(0),
+            Array(10).fill(0),
+            Array(10).fill(0),
+            Array(10).fill(0),
+        ]
+    };
+
+    updateMatrixItemState = (x, y) => {
+        const arr = this.state.arr;
+        arr[x][y] = Number(arr[x][y]) === 1 ? 0 : 1;
+        this.setState({
+            arr: arr
+        });
+    };
+
+    updateCount = (count) => {
+        this.setState({ domainCount: count });
     };
 
     updateMatrixWidth = (width) => {
@@ -23,6 +49,71 @@ export default class App extends React.Component {
         this.setState({ probability: probability });
     };
 
+    fill(data, x, y) {
+        let dom = [];
+        const flow = function (x, y) {
+            if (x >= 0 && y < data.length && y >= 0 && x < data[y].length) {
+                if (Number(data[y][x]) === 1) {
+                    data[y][x] = 0;
+                    dom.push({x: x, y: y});
+                    flow(x - 1, y);
+                    flow(x + 1, y);
+                    flow(x, y - 1);
+                    flow(x, y + 1);
+                }
+            }
+        };
+        flow(x, y);
+        if (dom.length > 0) {
+            return dom
+        }
+    }
+
+    // getRandomColor = () => {
+    //     const num = Math.random()*(255**3);
+    //     const r = Math.ceil(num % 255);
+    //     const g = Math.ceil(((num-r)/255)%255);
+    //     const b = Math.ceil(((((num-r)/255)-g)/255)%255);
+    //     return `#${this.dec2hex(r)}${this.dec2hex(g)}${this.dec2hex(b)}`
+    // };
+
+    // dec2hex = (decNum) => {
+    //     let hexNum = decNum.toString(16);
+    //     if (hexNum.length === 1){
+    //         hexNum = `0${hexNum}`;
+    //     }
+    //     return hexNum
+    // };
+
+    findDomains = () => {
+        let count = 0;
+        const domains = [];
+        const arr = this.state.arr;
+        for (const rowNumber in arr) {
+            for (const columnNumber in arr[rowNumber]) {
+                if (Number(arr[rowNumber][columnNumber]) === 1) {
+                    domains.push(this.fill(arr, parseInt(columnNumber), parseInt(rowNumber)));
+                    count++;
+                }
+            }
+        }
+
+        this.updateCount(count);
+
+        return domains
+    };
+
+    showDomains = () => {
+        const domains = this.findDomains();
+        console.log(this.state.domainCount);
+        console.log(this.state.arr);
+        console.log(domains);
+    };
+
+    result = () => {
+        this.showDomains();
+    };
+
     render() {
         return (
             <div className="container">
@@ -34,13 +125,13 @@ export default class App extends React.Component {
                     </div>
                     <div className="col-12 col-lg-6">
                         <button type="button" className="btn btn-outline-secondary btn-active">Построение матрицы</button>
-                        <button type="button" className="btn btn-outline-secondary btn-active">Подсчет доменов в матрице</button>
+                        <button type="button" className="btn btn-outline-secondary btn-active" onClick={this.result}>Подсчет доменов в матрице</button>
                         <button type="button" className="btn btn-outline-secondary btn-active">Автозаполнение</button>
                     </div>
                 </div>
                 <div className="row mt-3 text-center">
                     <div className="col-12">
-                        <Matrix width={this.state.matrixWidth} height={this.state.matrixHeight}/>
+                        <Matrix arr={this.state.arr} onClick={this.updateMatrixItemState}/>
                     </div>
                 </div>
             </div>
--- src/components/matrix/matrix.js
+++ src/components/matrix/matrix.js
@@ -2,40 +2,17 @@ import React from 'react';
 import MatrixItem from "./matrix-item";
 
 export default class Matrix extends React.Component {
-    state = {
-        test: Array(this.props.height).fill(Array(this.props.width).fill(0)),
-
-        arr: [
-            Array(this.props.width).fill(0),
-            Array(this.props.width).fill(0),
-            Array(this.props.width).fill(0),
-            Array(this.props.width).fill(0),
-            Array(this.props.width).fill(0),
-            Array(this.props.width).fill(0),
-            Array(this.props.width).fill(0),
-            Array(this.props.width).fill(0),
-            Array(this.props.width).fill(0),
-            Array(this.props.width).fill(0),
-        ]
-    };
-
-    updateMatrixItemState = (x, y) => {
-        const arr = this.state.arr;
-        arr[x][y] = Number(arr[x][y]) === 1 ? 0 : 1;
-        this.setState({
-            arr: arr
-        });
-    };
-
     render() {
+        const {arr, onClick} = this.props;
+
         const makeColumns = (row, rowNum) => {
             return row.map((col, colNum) => {
-                return <MatrixItem x={rowNum} y={colNum} col={col} onClick={this.updateMatrixItemState}/>
+                return <MatrixItem x={rowNum} y={colNum} col={col} onClick={onClick}/>
             });
         };
 
-        const tableTemplate = this.state.arr.map((row, rowNum) => {
-            return <tr key={rowNum}>{makeColumns(row, rowNum)}</tr>
+        const tableTemplate = arr.map((row, rowNum) => {
+            return <tr key={"tr" + rowNum}>{makeColumns(row, rowNum)}</tr>
         });
 
         return (
